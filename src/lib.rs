c823acbe8ae9b709c62070ac483ec7f4e7e26aae
LPTimey <tim.ruland@icloud.com>
2023-01-09 14:16:38 +0100

    added new func but not complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,53 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    self, parse_macro_input, Data::Struct, DataStruct, DeriveInput, Field, Fields::Named,
+    self, parse_macro_input, Data::Struct, DataStruct, DeriveInput, Fields::Named,
     FieldsNamed,
 };
 
-#[proc_macro_derive(StructFieldsIter)]
-pub fn derive_struct_fields_iter(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
+#[proc_macro_derive(StructIterTools)]
+pub fn derive_struct_iter_tools(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    println!("{:?}", ast);
+    let DeriveInput { ident, data, .. } = ast.clone();
 
-    let fields = if let Struct(DataStruct {
-        fields: Named(FieldsNamed { ref named, .. }),
-        ..
-    }) = data
-    {
-        named
-    } else {
-        panic!();
+    let fields = match data {
+        Struct(DataStruct {
+            fields: Named(FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => todo!(),
     };
 
-    let fields_vec: std::vec::Vec<std::string::String> = fields.iter().filter_map(|field| {match &field.ident {
-        Some(id) => Some(format!("{}",id)),
-        None => None,
-    }}).collect::<Vec<String>>();
+    println!("{:?}", fields);
 
+    let field_ids = fields
+        .clone()
+        .into_iter()
+        .filter_map(|field| match field.ident {
+            Some(id) => Some(id),
+            None => None,
+        })
+        .collect::<Vec<syn::Ident>>();
+
+    let mut field_types = fields.clone().into_iter().filter_map(|fields|{
+        match fields.ty{
+            syn::Type::Path(type_path) => Some(type_path.path.segments[0].clone().ident.to_string()),
+            _ => None,
+        }
+    }).collect::<Vec<String>>();
+    field_types.sort();
+    field_types.dedup();
+
+    println!("{:?}",field_types);
+
+    let fields_vec: std::vec::Vec<std::string::String> = fields
+        .iter()
+        .filter_map(|field| match &field.ident {
+            Some(id) => Some(format!("{}", id)),
+            None => None,
+        })
+        .collect::<Vec<String>>();
 
     let result = quote! {
         impl #ident {
@@ -31,7 +55,15 @@ pub fn derive_struct_fields_iter(input: TokenStream) -> TokenStream {
                 vec![#(#fields_vec.to_string()),*]
             }
         }
+        impl #ident{
+            pub fn values<E>(&self) -> ::std::vec::Vec<E>
+            where
+            E: Display + From<u64> + From<String>// + #(From<#field_types>)+*
+            {
+                vec![#(E::from(self.#field_ids.clone())),*]
+            }
+
+        }
     };
     result.into()
 }
-
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,11 +1,41 @@
-use struct_tools_derive::{StructFieldsIter};
+use std::fmt::Display;
 
-#[derive(StructFieldsIter)]
+use struct_tools_derive::StructIterTools;
+
+#[derive(StructIterTools)]
 pub struct Book {
     id: u64,
     title: String,
     pages: u64,
     author: String,
+}
+impl Book {
+    pub fn test<E>(&self) -> E
+    where
+        E: From<u64>,
+    {
+        E::from(self.id)
+    }
+}
+#[derive(Debug)]
+pub enum BookEnum {
+    U64(u64),
+    String(String),
+}
+impl From<u64> for BookEnum {
+    fn from(value: u64) -> Self {
+        BookEnum::U64(value)
+    }
+}
+impl From<String> for BookEnum {
+    fn from(value: String) -> Self {
+        BookEnum::String(value)
+    }
+}
+impl Display for BookEnum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[test]
@@ -13,4 +43,16 @@ fn fields() {
     let fields = Book::fields();
     println!("fields: {:?}", fields);
     assert_eq!(vec!["id", "title", "pages", "author"], fields)
+}
+
+#[test]
+fn values() {
+    let book = Book {
+        id: 1,
+        title: "Title".to_string(),
+        pages: 100,
+        author: "me".to_string(),
+    };
+    let book_values: Vec<BookEnum> = book.values::<BookEnum>();
+    println!("fields: {:?}", book_values);
 }
