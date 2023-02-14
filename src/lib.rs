d45d824a2f476993ca6f7f6bab1e6afbf5d7a37a
LPTimey <tim.ruland@icloud.com>
2023-02-14 14:11:30 +0100

    added extremely basic doc needs to be enhanced further

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,95 @@ use syn::{
 };
 
 #[proc_macro_derive(StructIterTools)]
+#[doc = r#"If you have a struct
+
+```rust
+pub struct Foo{
+    field1: i32,
+    field2: String,
+    {...}
+}
+```
+
+you can just add the derive to it
+
+```rust
+use struct_tools_derive::StructIterTools;
+
+#[derive(StructIterTools)]
+pub struct Foo{
+    field1: i32,
+    field2: String,
+    {...}
+}
+```
+
+This now let's you use it like this:
+
+#### fields
+
+```rust
+use struct_tools_derive::StructIterTools;
+
+let fields = Foo::fields();
+assert_eq!(fields,vec![String::from("field1"), String::from("field2"),...])
+```
+
+#### values
+
+```rust
+use struct_tools_derive::StructIterTools;
+
+enum FooEnum {
+  I32(i32),
+  String(String),
+  {...}
+}
+impl From<i32> for FooEnum {
+    fn from(value: 32) -> Self {
+        FooEnum::I32(value)
+    }
+}
+impl From<String> for FooEnum {
+    fn from(value: String) -> Self {
+        FooEnum::String(value)
+    }
+}
+
+let instance = Foo::default();
+
+let values = instance.values::<FooEnum>();
+
+assert_eq!(values,vec![FooEnum::I32(0), FooEnum::String(String::new()),...])
+```
+
+#### fields and values
+
+```rust
+use struct_tools_derive::StructIterTools;
+
+enum FooEnum {
+  I32(i32),
+  String(String),
+  {...}
+}
+impl From<i32> for FooEnum {
+    fn from(value: 32) -> Self {
+        FooEnum::I32(value)
+    }
+}
+impl From<String> for FooEnum {
+    fn from(value: String) -> Self {
+        FooEnum::String(value)
+    }
+}
+
+let instance = Foo::default();
+
+let f_v = instance.fields_and_values::<FooEnum>();
+
+assert_eq!(f_v,vec![(String::from("field1"), FooEnum::I32(0)), (String::from("field2"), FooEnum::String(String::new())),...])
+```"#]
 pub fn derive_struct_iter_tools(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
 
@@ -33,11 +122,48 @@ pub fn derive_struct_iter_tools(input: TokenStream) -> TokenStream {
 
     let result = quote! {
         impl #ident {
+            /**
+```rust
+use struct_tools_derive::StructIterTools;
+
+let fields = Foo::fields();
+assert_eq!(fields,vec![String::from("field1"), String::from("field2"),...])
+```
+            */
             pub fn fields() -> ::std::vec::Vec<::std::string::String>{
                 vec![#(#fields_vec.to_string()),*]
             }
         }
         impl #ident{
+            /**
+
+```rust
+use struct_tools_derive::StructIterTools;
+
+enum FooEnum {
+  I32(i32),
+  String(String),
+  {...}
+}
+impl From<i32> for FooEnum {
+    fn from(value: 32) -> Self {
+        FooEnum::I32(value)
+    }
+}
+impl From<String> for FooEnum {
+    fn from(value: String) -> Self {
+        FooEnum::String(value)
+    }
+}
+
+let instance = Foo::default();
+
+let values = instance.values::<FooEnum>();
+
+assert_eq!(values,vec![FooEnum::I32(0), FooEnum::String(String::new()),...])
+```
+
+            */
             pub fn values<E>(&self) -> ::std::vec::Vec<E>
             where
             E: #types
@@ -46,6 +172,33 @@ pub fn derive_struct_iter_tools(input: TokenStream) -> TokenStream {
             }
         }
         impl #ident{
+            /**
+```rust
+use struct_tools_derive::StructIterTools;
+
+enum FooEnum {
+  I32(i32),
+  String(String),
+  {...}
+}
+impl From<i32> for FooEnum {
+    fn from(value: 32) -> Self {
+        FooEnum::I32(value)
+    }
+}
+impl From<String> for FooEnum {
+    fn from(value: String) -> Self {
+        FooEnum::String(value)
+    }
+}
+
+let instance = Foo::default();
+
+let f_v = instance.fields_and_values::<FooEnum>();
+
+assert_eq!(f_v,vec![(String::from("field1"), FooEnum::I32(0)), (String::from("field2"), FooEnum::String(String::new())),...])
+```
+            */
             pub fn fields_and_values<E>(&self) -> ::std::vec::Vec<(::std::string::String, E)>
             where
             E: #types
@@ -61,6 +214,58 @@ pub fn derive_struct_iter_tools(input: TokenStream) -> TokenStream {
 }
 
 #[proc_macro_derive(StructEnum, attributes(EnumDerives))]
+#[doc = r"
+Will create an Enum which is capable of containing all possible contents of the struct
+
+# Example
+
+```rust
+use struct_tools_derive::StructEnum;
+
+#[derive(StructEnum)]
+pub struct Foo{
+    field1: i32,
+    field2: String,
+    {...}
+}
+```
+
+This Grants you access to an automatically generated Enum with the name `{structname}Enum`.
+
+its Variants are named by Capitalizing the first letter of the respective Type
+
+```rust
+pub enum FooEnum{
+    I32(i32),
+    String(String),
+    {...}
+}
+impl From<u64> for FooEnum {
+    fn from(value: u64) -> Self {
+        FooEnum::U64(value)
+    }
+}
+impl From<String> for FooEnum {
+    fn from(value: String) -> Self {
+        FooEnum::String(value)
+    }
+}
+{...}
+```
+
+This means, that it can be used with both values and fields and values from StructIterTools
+
+you can also have it derive traits by adding them to the `EnumDerives` attribute like this:
+
+```rust
+#[derive(StructEnum)]
+#[EnumDerives(Debug)]
+pub struct Foo{
+    field1: i32,
+    field2: String,
+    {...}
+}
+```"]
 pub fn derive_struct_enum(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let DeriveInput {
